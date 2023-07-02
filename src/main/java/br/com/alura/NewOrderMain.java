8fa1c7ee2eee94a62efb4881cceab3a4749f9a8f
Alexandre Oliveira <alexmdo@gmail.com>
2023-07-02 13:24:23 -0300

    Serialização com GSON

--- src/main/java/br/com/alura/NewOrderMain.java
+++ src/main/java/br/com/alura/NewOrderMain.java
@@ -2,22 +2,26 @@ package br.com.alura;
 
 import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
-        try (var dispatcher = new KafkaDispatcher()) {
-            for (int i = 0; i < 100; i++) {
-                var key = UUID.randomUUID().toString();
+        try (var orderKafkaDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailKafkaDispatcher = new KafkaDispatcher<String>()) {
+                for (int i = 0; i < 100; i++) {
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    BigDecimal amount = new BigDecimal(Math.random() * 5000 + 1);
+                    var order = new Order(userId, orderId, amount);
 
-                var value = key + ",213,12312";
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                    orderKafkaDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
 
-                var email = "Thank you for your order! We are processing your order!";
-                var emailRecord = new ProducerRecord<>("", key, email);
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, email);
+                    var email = "Thank you for your order! We are processing your order!";
+                    emailKafkaDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
